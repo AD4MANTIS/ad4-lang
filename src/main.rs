a755e9d1f125403e14f0acfb62916c8ea0d69d43
AD4MANTIS <ad4mantis@gmail.com>
2025-05-22 21:24:32 +0200

    Better error output

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use color_eyre::eyre::eyre;
 pub use expression::*;
 pub use lexer::*;
 pub use literal::*;
 pub use statement::*;
 use std::{
     collections::HashMap,
+    env::args,
     io::{Write, stdin, stdout},
 };
 pub use value::*;
@@ -15,7 +17,9 @@ mod statement;
 mod value;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
+    let debug = args().any(|args| matches!(args.as_str(), "--debug" | "-d"));
+    color_eyre::install()?;
+
     let variables = HashMap::<String, Value>::new();
 
     let mut input = String::new();
@@ -31,7 +35,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         let mut lexer = Lexer::build(&input);
         let expr = Expression::parse(&mut lexer, 0);
-        println!("{:?}", expr.eval(&variables)?);
+        let result = expr.eval(&variables).map_err(|e| eyre!(e));
+
+        if debug {
+            match result {
+                Ok(result) => println!("{result:?}"),
+                Err(e) => println!("{e:?}"),
+            }
+        } else {
+            match result {
+                Ok(result) => println!("{result}"),
+                Err(e) => println!("{e}"),
+            }
+        }
     }
 
     Ok(())
