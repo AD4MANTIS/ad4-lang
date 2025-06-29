bb61a4bafd6fe0ee41ed2218a669cf1e1c84f60b
AD4MANTIS <ad4mantis@gmail.com>
2025-06-29 20:54:23 +0200

    Run Files

--- ad4-lang/src/main.rs
+++ ad4-lang/src/main.rs
@@ -2,7 +2,10 @@ use prelude::*;
 use std::{
     collections::HashMap,
     env::args,
+    fs::read_to_string,
     io::{Write, stdin, stdout},
+    path::PathBuf,
+    str::FromStr,
 };
 
 mod block;
@@ -19,50 +22,70 @@ mod variable;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let debug = args().any(|args| matches!(args.as_str(), "--debug" | "-d"));
+
     color_eyre::install()?;
 
     let mut variables = HashMap::<Variable, Value>::new();
+
+    let code_str = args()
+        .skip(1) // skip the executable name
+        .filter_map(|arg| PathBuf::from_str(&arg).ok())
+        .find(|path| path.is_file())
+        .map(|path| read_to_string(path).unwrap());
+
+    if let Some(code_str) = code_str {
+        execute_code(&format!("{{{code_str}}}"), &mut variables, debug);
+        return Ok(());
+    }
 
     let mut input = String::new();
     loop {
         input.clear();
 
-        loop {
-            print!(">> ");
-            stdout().flush().unwrap();
-            stdin().read_line(&mut input).unwrap();
-
-            if are_brackets_closed(&input, Operator::OpeningBracket)
-                && are_brackets_closed(&input, Operator::OpeningCurlyBrace)
-            {
-                break;
-            }
-        }
+        read_from_console(&mut input);
 
         if input.trim() == "exit" {
             break;
         }
 
-        macro_rules! print_err {
-            { $expr:expr } => {
-                match $expr {
-                    Ok(x) => x,
-                    Err(e) => {
-                        print_cond(e, debug);
-                        continue;
-                    }
-                }
-            };
-        }
-
-        let mut lexer = print_err! { Lexer::build(&input) };
-        let expr = print_err! { Statement::parse(&mut lexer) };
-        let result = print_err! { expr.execute(&mut variables) };
-
-        print_cond(result, debug);
+        execute_code(&input, &mut variables, debug);
     }
 
     Ok(())
+}
+
+fn execute_code(input: &str, variables: &mut HashMap<Variable, Value>, debug: bool) {
+    macro_rules! print_err {
+        { $expr:expr } => {
+            match $expr {
+                Ok(x) => x,
+                Err(e) => {
+                    print_cond(e, debug);
+                    return;
+                }
+            }
+        };
+    }
+
+    let mut lexer = print_err! { Lexer::build(input) };
+    let expr = print_err! { Statement::parse(&mut lexer) };
+    let result = print_err! { expr.execute(variables) };
+
+    print_cond(result, debug);
+}
+
+fn read_from_console(input: &mut String) {
+    loop {
+        print!(">> ");
+        stdout().flush().unwrap();
+        stdin().read_line(input).unwrap();
+
+        if are_brackets_closed(&*input, Operator::OpeningBracket)
+            && are_brackets_closed(&*input, Operator::OpeningCurlyBrace)
+        {
+            break;
+        }
+    }
 }
 
 fn print_cond<T: std::fmt::Display + std::fmt::Debug>(val: T, debug: bool) {
